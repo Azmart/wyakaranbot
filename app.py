f73fcddb472cf612d77d7765bd7cc8acc06357f8
marta <20216610@life.hkbu.edu.hk>
2024-07-28 01:46:17 +0800

    github3

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ st.set_page_config(page_title="मेरो व्याकरण गुरू�
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 hide_github_icon = """
+    <style>
     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; } 
+    </style>
     """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
